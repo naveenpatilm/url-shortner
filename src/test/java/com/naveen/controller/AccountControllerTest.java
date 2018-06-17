c015ba5900ddb922b039925e18048bef16472893
apple <naveen.patil@weboniselab.com>
2018-06-17 19:31:33 +0530

    update test cases for account controller

--- src/test/java/com/naveen/controller/AccountControllerTest.java
+++ src/test/java/com/naveen/controller/AccountControllerTest.java
@@ -1,11 +1,17 @@
 package com.naveen.controller;
 
 import com.naveen.UrlShortnerApplication;
+import com.naveen.request.OpenAccountRequest;
+import com.naveen.response.OpenAccountResponse;
+import com.naveen.service.impl.AccountServiceImpl;
+import com.naveen.util.ApplicationConstants;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,10 +19,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -24,6 +31,10 @@ public class AccountControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private WebApplicationContext webApplicationContext;
+    @Mock
+    private AccountServiceImpl accountService;
+    @InjectMocks
+    private AccountController accountController;
 
     @Before
     public void setUp() throws Exception {
@@ -46,5 +57,34 @@ public class AccountControllerTest {
                 .content("")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void OpenAccount_newAccount_success() {
+        OpenAccountRequest openAccountRequest = new OpenAccountRequest();
+        openAccountRequest.setAccountId("testAccount");
+
+        OpenAccountResponse expectedOpenAccountResponse = new OpenAccountResponse(true,
+                ApplicationConstants.ACCOUNT_OPENED, "abcd123");
+
+        when(accountService.openAccount(openAccountRequest.getAccountId())).thenReturn(expectedOpenAccountResponse);
+
+        OpenAccountResponse actualOpenAccountResponse = accountController.OpenAccount(openAccountRequest);
+
+        assertEquals(expectedOpenAccountResponse, actualOpenAccountResponse);
+    }
+
+    @Test
+    public void OpenAccount_sameAccountOpen_throwIllegalArgumentException() {
+        OpenAccountRequest openAccountRequest = new OpenAccountRequest();
+        openAccountRequest.setAccountId("testAccount");
+
+        accountController.OpenAccount(openAccountRequest);
+        try {
+            accountController.OpenAccount(openAccountRequest);
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
+
     }
 }
