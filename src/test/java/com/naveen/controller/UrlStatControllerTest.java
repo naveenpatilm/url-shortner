2df08920fda326510c3531e7cfb385ec96a4e36b
apple <naveen.patil@weboniselab.com>
2018-06-17 20:53:50 +0530

    add test cases for url stats controller

---
+++ src/test/java/com/naveen/controller/UrlStatControllerTest.java
@@ -0,0 +1,68 @@
+package com.naveen.controller;
+
+import com.naveen.request.OpenAccountRequest;
+import com.naveen.request.UrlRegistrationRequest;
+import com.naveen.response.OpenAccountResponse;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UrlStatControllerTest {
+    private MockMvc mockMvc;
+    @Autowired
+    private AccountController accountController;
+    @Autowired
+    private UrlRegistrationController urlRegistrationController;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private OpenAccountResponse openAccountResponse;
+    private static boolean isSetUpDone = false;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    public void seedRegistrationData() throws IllegalAccessException {
+        if(isSetUpDone) return;
+        OpenAccountRequest openAccountRequest = new OpenAccountRequest();
+        openAccountRequest.setAccountId("testAccount");
+        openAccountResponse = accountController.OpenAccount(openAccountRequest);
+        UrlRegistrationRequest urlRegistrationRequest = new UrlRegistrationRequest();
+        urlRegistrationRequest.setUrl("www.longurl.com");
+        urlRegistrationRequest.setRedirectType(301);
+        urlRegistrationController.registerUrl(openAccountResponse.getPassword(), urlRegistrationRequest);
+        isSetUpDone = true;
+    }
+
+    @Test
+    public void getUrlStats_authrorizedRequest() throws Exception {
+        seedRegistrationData();
+        mockMvc.perform(get("/statistic/testAccount")
+                .header("Authorization", openAccountResponse.getPassword())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getUrlStats_unauthrorizedRequest() throws Exception {
+        mockMvc.perform(get("/statistic/account-id")
+                .header("Authorization", "invalid_auth_token")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized());
+    }
+}
